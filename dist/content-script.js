fcd2839bbd90d3d0eb1c5f44d9b87fcb4451538f
Omi Khan <mehboobehsankhan@gmail.com>
2024-09-23 04:25:49 +0600

    Added background.js to fix the google icon problem

--- dist/content-script.js
+++ dist/content-script.js
@@ -3,12 +3,17 @@ chrome.storage.sync.get(["dagrind_enable"]).then((res) => {
     chrome.storage.sync.get(["dagrind_list"]).then((res) => {
       if (
         res.dagrind_list.some((e) => {
-          return location.href.toLowerCase().includes(e[0].toLowerCase() + ".com") && e[1];
+          return (
+            location.href.toLowerCase().includes(e[0].toLowerCase() + ".com") &&
+            e[1]
+          );
         })
       ) {
-        alert("Redirecting in a bit!!");
         if (location.href !== "https://www.google.com/") {
-          location.replace("https://www.google.com/");
+          setTimeout(() => {
+            chrome.runtime.sendMessage({ closeThis: true });
+          }, 3000);
+          alert("💡 Stop loosing focus! 💡\nClosing this tab in 3 seconds.");
         }
       }
     });
